df334e01a24cfc044d8d93cf49a690e3d28f16e3
lyfer233 <2217636939@qq.com>
2021-07-29 16:54:11 +0800

    first

---
+++ 28.Implement strStr().js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if (needle.length === 0) return 0;
+    answer = -1;
+    for (let i = 0; i < haystack.length; i++) {
+        flag = true;
+        if (i + needle.length >= haystack.length) break;
+        for (let j = 0; j < needle.length; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            answer = i;
+            break;
+        }
+    }
+    return answer;
+};
