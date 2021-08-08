9a4050bede0d968a48e20ae1a2f0b00b07a28e78
lyfer233 <2217636939@qq.com>
2021-08-08 23:58:01 +0800

    daily problem~

---
+++ NthTribonacciNumber.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int tribonacci(int n) {
+        int[] nums = new int[50];
+        nums[0] = 0;
+        nums[1] = nums[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            nums[i] = nums[i - 1] + nums[i - 2] + nums[i - 3];
+        }
+        return nums[n];
+    }
+}
